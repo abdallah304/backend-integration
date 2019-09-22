773767ffc8af33d827609e024357e5dee1e44436
tariq <tariqabdelghani@gmail.com>
2019-09-22 07:45:43 +0200

    added news update delete and not found

--- unesco-backend-project/routes/News.js
+++ unesco-backend-project/routes/News.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express('Router');
-const mongoose= require('mongoose');
 
 
 const NewsModel = require('../models/News')
@@ -19,28 +18,23 @@ router.get('/',function(req,res,next)
     });
     
 });
-//
+
 // news detail
-router.get('/:id',(req,res,)=>{
+router.get('/:id',(req,res,next)=>{
     NewsModel.find({_id:req.params.id},(err, docs)=>{
         if(!err){
             res.send(docs);
-        }else{
+        }else if (err.name == 'CastError'){
+            //res.status(404);
+            //res.send(err);
+            next();
+        }else {
+            res.status(404);
             res.send(err);
         }
     });
 });
-//
-// specific news / news detail
-router.get('/department/:id',(req,res,)=>{
-    NewsModel.find({department_id:req.params.id},(err, docs)=>{
-        if(!err){
-            res.send(docs);
-        }else{
-            res.send(err);
-        }
-    });
-});
+
 // get all specific to department
 router.get('/department/:id',(req,res,)=>{
     NewsModel.find({department_id:req.params.id},(err, docs)=>{
@@ -51,20 +45,52 @@ router.get('/department/:id',(req,res,)=>{
         }
     });
 });
+// get all specific to subdepartment
 
+// update / modify
+// update
+// we replace the whole document here so
+// make sure to send full object data.
+router.put('/:id',(req, res)=>{
+    const data = req.body;
+    NewsModel.replaceOne(
+        {_id:req.params.id},
+        data, 
+        (err, doc)=>{
+            if(!err) res.send(doc);
+            else {
+                res.status(405);
+                res.send(err);
+            }
+        });
+});
 
-//
+// delete
+router.delete('/:id', (req, res)=>{
+    NewsModel.deleteOne({_id:req.params.id}, (err)=>{
+        if (!err)
+            res.send('deleted successfully');
+        else
+            res.send(err);
+        
+    });
+});
+
+// create news
 router.post('/',(req,res)=>{
     const data = req.body;
     const News = new NewsModel(data);
     News.save((err,doc)=>{
 
         if(!err)res.send('ok in saving')
-        else res.send('erroe in saving')
+        else res.send(err)
     })
 });
 
-
-   
+// not found
+router.all('**',(req, res)=>{
+    res.status(404);
+    res.send('page not found');
+});
 
     module.exports=router;
